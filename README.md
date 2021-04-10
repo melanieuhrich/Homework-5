5391c1462ac1fd6f3172843c3af31842d52fabf7
mel <muhrich6@gmail.com>
2021-04-10 16:37:57 -0500

    all done

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Homework 5
+
+# Purpose of the Assignment 
+
+Create a workday schedule with a timeblock for every hour that changes colors for past, present, and future and allows the user to save their tasks to local storage. 
+
+# What I Did and Why
+
+Lines 1-2: Creates the today variable and defines it as the current moment in MMMM Do, YYYY format. Links it with the currentDay ID in the HTML and turns the today variable into text so that it displays in the correct spot. 
+
+Lines 4-5: Creates the textArea variable and links it to the textArea class in the HTML. Gives the textArea an attibute of 'enabled' so that the user can type their task in it. 
+
+Line 7: Calls the renderLocalStorage function.
+
+Line 9: Calls the getHour function. 
+
+Lines 11-19: Creates variables savedContent9-savedContent17. Defines them as the value of their corresponding text areas in the HTML. 
+
+Lines 21-31: Declares the renderLocalStorage function. Tells it to find the IDs for each hour in the HTML and their textArea class, then to get the value of each item from localStorage according to their key. 
+
+Lines 33-76: Finds the saveBtn class in the HTML. On a click, reminds the computer of the variables savedContent9-savedContent17. Then sets the item to localStorage according to its unique key and value.
+
+Lines 78-93: Declares the getHour function. Tells it to find the row2 class in the HTML then creates an each loop. Creates the hour variable and defines it as the current moment in hour format. Creates the myHour variable, links it with the data-hour attribute in the HTML, and parses it. If myHour is less than hour, give the data-hour a class of past. If myHour is equal to hour, give the data-hour a class of present and remove the past class. Otherwise (if myHour is greater than hour), give the data-hour a class of future and remove the present and past classes. 
+
+# Screenshot
+
+![screenshot](screenshot.png)
+
+# Link to Deployed Application 
+
+https://melanieuhrich.github.io/Homework-5/
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var today = moment().format('MMMM Do, YYYY');
 $('#currentDay').text(today);
-console.log('now');
 
 var textArea = document.querySelectorAll('.textArea');
 $('textArea').attr('enabled', 'enabled');
 
 renderLocalStorage();
+
 getHour();
 
 var savedContent9 = $('#9task').val();
@@ -30,58 +30,48 @@ function renderLocalStorage(){
     $('#5PM .textArea').val(localStorage.getItem('5PM'));
 }
 
-
 $('.saveBtn').on('click', function(){
     var savedContent9 = $('#9task').val();
-    console.log(savedContent9);
     localStorage.setItem('9AM', savedContent9);
 });
 
 $('.saveBtn').on('click', function(){
     var savedContent10 = $('#10task').val();
-    console.log(savedContent10);
     localStorage.setItem('10AM', savedContent10);
 });
 
 $('.saveBtn').on('click', function(){
     var savedContent11 = $('#11task').val();
-    console.log(savedContent11);
     localStorage.setItem('11AM', savedContent11);
 });
 
 $('.saveBtn').on('click', function(){
     var savedContent12 = $('#12task').val();
-    console.log(savedContent12);
     localStorage.setItem('12PM', savedContent12);
 });
 
 $('.saveBtn').on('click', function(){
     var savedContent13 = $('#13task').val();
-    console.log(savedContent13);
     localStorage.setItem('1PM', savedContent13);
 });
 
 $('.saveBtn').on('click', function(){
     var savedContent14 = $('#14task').val();
-    console.log(savedContent14);
     localStorage.setItem('2PM', savedContent14);
 });
 
 $('.saveBtn').on('click', function(){
     var savedContent15 = $('#15task').val();
-    console.log(savedContent15);
     localStorage.setItem('3PM', savedContent15);
 });
 
 $('.saveBtn').on('click', function(){
     var savedContent16 = $('#16task').val();
-    console.log(savedContent16);
     localStorage.setItem('4PM', savedContent16);
 });
 
 $('.saveBtn').on('click', function(){
     var savedContent17 = $('#17task').val();
-    console.log(savedContent17);
     localStorage.setItem('5PM', savedContent17);
 });
 
@@ -89,11 +79,8 @@ function getHour(){
 $('.row2').each(function () {
   var hour = moment().hours();
   var myHour = parseInt($(this).attr('data-hour'));
-  console.log(myHour);
-  console.log(hour);
   if (myHour < hour) {
     $(this).addClass('past');
-    console.log('hour');
   } else if (myHour === hour) {
     $(this).addClass('present');
     $(this).removeClass('past');
